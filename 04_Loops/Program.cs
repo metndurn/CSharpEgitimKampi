da280857788dc2f8d42f2dda52e19fda42e26726
metndurn <mtndrn123@gmail.com>
2024-10-15 21:09:13 +0300

    geri kalan donguleri ve uygulamali olan bolumleridir..

--- 04_Loops/Program.cs
+++ 04_Loops/Program.cs
@@ -10,6 +10,175 @@ namespace _04_Loops
     {
         static void Main(string[] args)
         {
+            #region For Dongusu
+
+            /*donguler programlamada bu sekılde kodlar yazılmaz 1+2+3+4+5+6 
+             * bu sekılde yazılırsa normal toplama gibi dusunulur ama programlarda
+             * dogru kullanım olmayacaktır
+             
+            3+3=6+4=10+5=15+6=21
+            bu sekılde oldugu zaman sistem toplamaya odaklanır ve sistem dogru calısmaz
+            
+            for yapısı su sekıldedır for (x;y;z)
+            burada 
+            x: baslangıc degeridir ve onu tutar
+            y: bitis degerini tutar
+            z: artis veya azalis degerini tutar
+            parantez icinede kodları yazıp program calısır hale getırılır
+             */
+
+            /*ekrana 5 kere yazdırdık*/
+            //int i;
+            //for (i = 1; i <=5; i++) 
+            //{
+            //    Console.WriteLine("C# Egitim Kampi:");
+            //}
+
+            /*ekrana 1 den 20 kadar kucukten buyuge sıralı olacak sekılde yazdırdık*/
+            //for (int i = 1; i <=20; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            /*burada sayıları 3'lü halde yazdırmasını sagladık yani ekrana 3,6,9 seklınde yazacaktır*/
+            //for (int i = 3; i <= 50; i +=3)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            /*burada ekranda kullanıcıdan girdiler yaptırarak ekrana yazdırdık*/
+            //Console.Write("Lutfen ekrana yazilmasini istediginiz adedi yaziniz.: ");
+            //int finishValue = int.Parse(Console.ReadLine());
+
+            //for(int i=1; i <= finishValue; i++)
+            //{
+            //    Console.WriteLine("Yasasin Devleti Aliyye.: ");
+            //}
+
+
+            #endregion
+
+            #region For Dongusu Ile Karar Yapilari
+            /*burdaki dongude 1 ile 100 arasındaki sayılardan 5'e tam bolunen
+             sayıları bulalım*/
+            //for (int i = 1; i <= 100; i++)//1'den 100 kadar sayılar verıldı 
+            //{
+            //    if (i % 5 == 0)//verilen sayılar 5'e bolumunden kalanı verır
+            //    {
+            //        Console.WriteLine(i);//ekrana yazdırılır
+            //    }
+            //}
+
+            /*1 ile 10 arasındaki ardısık sayıları toplayan uygulama*/
+
+            //int totalValue = 0;
+            //for(int i=1; i <= 10; i++)
+            //{
+            //    totalValue += i;
+            //}
+            //Console.WriteLine(totalValue);
+
+            /*burda 1 ile 20 arasında ki cift sayıların toplamını bulan uygulama*/
+
+            //int totalValue = 0;
+            //for(int i = 1; i <= 20; i++)
+            //{
+            //    if (i % 2 == 0)//sayının 2'ser 2'ser toplanarak ekrana yazdırılır ve daha sonra toplamı yazıldı
+            //    {
+            //        totalValue += i;//toplam yazıldı
+            //        Console.WriteLine(i);//her seferinde i degiskeni ekrana yazılacak
+            //    }
+            //}
+            //Console.WriteLine("------");
+            //Console.WriteLine(totalValue);//inttin degeri toplanıp buraya yazıldı
+
+            //int count = 0;
+            //for (int i = 1; i <= 50; i++) 
+            //{
+            //    if (i % 7 == 0)
+            //    {
+            //        count++;
+            //    }
+            //}
+            //Console.WriteLine(count);
+
+            /*her saat bası olusan bakteri mıktarını bulan kodu yazdık
+             * yani 1.ci saatte 2 tane 2.ci satte 4 tane oldugu varsayılarak bulunur
+             * 1-2-4-8-16-...*/
+
+            //int bacterium = 1;
+            //for(int i=1; i <= 24; i++)
+            //{
+            //    bacterium *= 2;
+            //    Console.WriteLine(i + ".Saat Sonunda: " + bacterium);
+            //}
+
+            #endregion
+
+            #region While Dongusu
+
+            /*while dongusu şu sekıldedır 
+            While(sart)
+            {
+            islemler yapılır
+            }
+            bitis
+            */
+
+            //int i = 1;
+            //while (i <= 10)
+            //{
+            //    Console.WriteLine("Merhaba Donguler..:");
+            //    i++;
+            //}
+
+
+            /*1 ile 10 arasında 3 bolunen tam sayıları bulalım*/
+
+            //int count = 1;
+            //while (count <= 10)
+            //{
+            //    if (count % 3 == 0)
+            //    {
+            //        Console.WriteLine(count);
+            //    }
+            //    count++;
+            //}
+
+            /*1 ile 10 arasındaki sayıların toplamını bulduk*/
+            //int i = 1;
+            //int sum = 0;
+
+            //while (i <= 10)
+            //{
+            //    sum += i;
+            //    i++;
+            //}
+            //Console.WriteLine(sum);
+            #endregion
+
+            #region Ornek Sınav Sorusu
+
+            /*Klavyeden girilen 3 basamaklı sayının basamakları toplamını hesaplayan
+             kodu yazınız mesele 456 yazıldı bunların toplamını bulacaksın*/
+
+            //456
+
+            Console.Write("Lutfen Sayıyı Giriniz.:");
+            int number=int.Parse(Console.ReadLine());
+            int ones, tens, hundreds, sum;
+
+            ones = number % 10;
+            tens = (number % 100) / 10;
+            hundreds = number / 100;//4.56 --> sadece 4 olanı verecek 56 vermeyecek
+
+            Console.WriteLine(ones + "-" + tens + "-" + hundreds);
+
+            sum = ones + tens + hundreds;
+            Console.WriteLine(sum);
+
+            #endregion
+            Console.ReadKey();
         }
     }
 }
---
+++ 05_LoopsWithsStars/Program.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_LoopsWithsStars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*burada donguler ile bol bol denemeler yapacagız*/
+            #region Alt Alta 10 Tane Yildiz Olusturma
+
+            //for(int i = 0; i <= 10; i++)
+            //{
+            //    Console.WriteLine("*");
+            //}
+
+            #endregion
+
+            #region Yan Yana 10 Tane Yildiz Olusturma
+
+            //for (int i = 0; i <= 10; i++)
+            //{
+            //    Console.Write("*");
+            //}
+
+            #endregion
+
+            #region Alt Alta 10 Tane Yildiz Olusturma Her Satirda 10 Tane Yildiz Olsun
+
+            //for (int i = 0; i <= 10; i++)
+            //{
+            //    Console.WriteLine("**********");
+            //}
+
+            #endregion
+
+            #region Dik Ucgen Olusturma
+            /*burada dık ucgen olustururken ilk for icinde 1'den baslatarak 5'e kadar
+            okuttukki ilk yıldızı olusturuyor ve daha sonra 2.ci for olusturuldugunda
+           j++ degıskenıne atama yapılıyor yanı once attırılıyor daha sonra i ile j 
+           denklestırılıyor ve yildizlar olusmus oluyor*/
+            //for (int i = 1; i <= 5; i++)
+            //{
+            //    for(int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            #endregion
+
+            #region Ters Dik Ucgen Olusturma
+
+            //for (int i = 5; i >=1; i--)
+            //{
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            #endregion
+
+            #region Dik ve Ters Dik Ucgen Beraber Olusturma
+
+            //for(int i = 1; i <= 5; i++)
+            //{
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            //for(int k = 4; k >= 1; k--)
+            //{
+            //    for (int m = 1; m <= k; m++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            #endregion
+
+            #region Baklava Dilimi ve Yildiz Olusturma
+
+            //sembolun üst kısmı
+            //int n = 5;
+            //for (int i = 1; i <= n; i++) 
+            //{
+            //    for (int j = n - 1; j > 0; j--)
+            //    {
+            //        Console.Write(" ");
+            //    }
+
+            //    for (int k = 1; k <= 2 * i - 1; k++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+            ////sembolun alt kısmı
+            //for (int i = n - 1; i >= 1; i--)
+            //{
+            //    for (int j = n - 1; j > 0; j--)
+            //    {
+            //        Console.Write(" ");
+            //    }
+
+            //    for (int k = 1; k <= 2 * i - 1; k++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            #endregion
+
+            #region Piramit ve Yildiz Olusturma
+
+            //int n = 5;
+            //for (int i = 1; i <= n; i++)
+            //{
+            //    //bosluklar ıcın olusturuldu
+            //    for (int j = n - i; j > 0; j--)
+            //    {
+            //        Console.Write(" ");
+            //    }
+
+            //    //yıldızlar ıcın olusturuldu
+            //    for (int k = 1; k <= 2 * i - 1; k++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            #endregion
+            
+            #region Ters Piramit ve Yildiz Olusturma
+
+            int n = 5;
+            for (int i = n; i >= 1; i--)
+            {
+                //bosluklar ıcın olusturuldu
+                for (int j = n - i; j > 0; j--)
+                {
+                    Console.Write(" ");
+                }
+
+                //yıldızlar ıcın olusturuldu
+                for (int k = 1; k <= 2 * i - 1; k++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+            #endregion
+            Console.ReadKey();
+        }
+    }
+}
